8fde8fdcaa0584f700a075b1521b784158e64bd9
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2014-03-15 12:16:49 +0700

    [4/4] Telephony: Smart Phone Call
    Put on your ear, and this will automatically call current number in message or dialer
    
    Change-Id: I561024cb4b94e5742c500469e46993a2afa9bf34
    Signed-off-by: cj360 <ayunker551@gmail.com>
    
    Conflicts:
    	res/values/strings_custom.xml
    	res/xml/call_feature_setting.xml
    	src/com/android/phone/CallFeaturesSetting.java

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -224,6 +224,7 @@ public class CallFeaturesSetting extends PreferenceActivity
             "button_choose_people_lookup_provider";
     private static final String BUTTON_CHOOSE_REVERSE_LOOKUP_PROVIDER =
             "button_choose_reverse_lookup_provider";
+    private static final String BUTTON_SMART_PHONE_CALL_KEY = "button_smart_phone_call";
 
     private Intent mContactListIntent;
 
@@ -325,6 +326,7 @@ public class CallFeaturesSetting extends PreferenceActivity
     private CheckBoxPreference mButtonAutoRetry;
     private CheckBoxPreference mButtonHAC;
     private CheckBoxPreference mButtonCallUiInBackground;
+    private CheckBoxPreference mSmartCall;
     private ListPreference mButtonDTMF;
     private ListPreference mButtonTTY;
     private CheckBoxPreference mButtonNoiseSuppression;
@@ -635,6 +637,10 @@ public class CallFeaturesSetting extends PreferenceActivity
                 // This should let the preference use default behavior in the xml.
                 return false;
             }
+        } else if (preference == mSmartCall){
+            Settings.AOSPA.putInt(getContentResolver(), Settings.AOSPA.SMART_PHONE_CALLER,
+                    mSmartCall.isChecked() ? 1 : 0);
+            return true;
         }
         return false;
     }
@@ -1729,6 +1735,10 @@ public class CallFeaturesSetting extends PreferenceActivity
         addOptionalPrefs(prefSet);
 
         onCreateLookupPrefs();
+
+        mSmartCall = (CheckBoxPreference) findPreference(BUTTON_SMART_PHONE_CALL_KEY);
+        mSmartCall.setChecked(Settings.AOSPA.getInt(getContentResolver(),
+                Settings.AOSPA.SMART_PHONE_CALLER, 0) != 0 ? true : false);
 
         // create intent to bring up contact list
         mContactListIntent = new Intent(Intent.ACTION_GET_CONTENT);
